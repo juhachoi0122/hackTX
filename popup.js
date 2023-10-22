91e2372d59bb6612e7f63fd01c7cce35976603c3
Juha <146126150+juhachoi0122@users.noreply.github.com>
2023-10-22 10:18:31 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,29 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const generateButton = document.getElementById('generate');
-    const inputTextarea = document.getElementById('input');
-    const summaryDiv = document.getElementById('summary');
-  
-    generateButton.addEventListener('click', async function() {
-      const inputText = inputTextarea.value;
-      if (inputText.trim() === '') {
-        summaryDiv.textContent = 'Enter text:';
-        return;
-      }
-  
-      const response = await fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': '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'
-        },
-        body: JSON.stringify({
-          'messages': [{'role': 'system', 'content': 'You: ' + inputText}, {'role': 'system', 'content': 'You: Generate a 10-word summary.'}],
-          'max_tokens': 10
-        })
-      });
-  
-      const data = await response.json();
-      const summary = data.choices[0].message.content;
-      summaryDiv.textContent = summary;
+  const generateButton = document.getElementById('generateSummary');
+  const inputTextarea = document.getElementById('userInput');
+  const summaryDiv = document.getElementById('summary');
+
+  generateButton.addEventListener('click', async function() {
+    const inputText = inputTextarea.value;
+    console.log("HAHAHAHAH "+inputText);
+    if (inputText.trim() === '') {
+      summaryDiv.textContent = summaryDiv;
+      return;
+    }
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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'
+      },
+      body: JSON.stringify({
+        'model': 'gpt-3.5-turbo', // Specify the model you want to use
+        'messages': [
+          {'role': 'system', 'content': 'You: ' + inputText},
+          {'role': 'system', 'content': 'You: Generate a 10-word summary.'}
+        ],
+        'max_tokens': 20
+      })
     });
-  });  
+    
+    console.log(JSON.stringify({
+      'messages': [{'role': 'system', 'content': 'You: ' + inputText}, {'role': 'system', 'content': 'You: Generate a 10-word summary.'}],
+      'max_tokens': 20
+    }));
+    const data = await response.json();
+console.log(data); // Log the data to the console
+
+// Access specific properties of the data object
+const summary = data.choices[0].message.content;
+console.log(summary); // Log the summary to the console
+summaryDiv.textContent = summary;
+    
+  });
+}); 
